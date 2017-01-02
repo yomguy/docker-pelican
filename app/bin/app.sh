b9624725feb7d8d4aac8441e5eb3ca6df2bd59c0
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2017-01-02 17:05:59 +0100

    rename conf

--- app/bin/app.sh
+++ app/bin/app.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ -f /var/in/pelicanconf.py ]; then
-    conf=/var/in/pelicanconf.py
+if [ -f /var/in/pelican_conf.py ]; then
+    conf=/var/in/pelican_conf.py
 else
-    conf=pelicanconf.py
+    conf=pelican_conf.py
 fi
 
 if [ "$1" = "--runserver" ]; then
